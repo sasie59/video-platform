2a615446cbf2095172e0514c7ed62894767823f0
sasie59 <sasie59@gmail.com>
2021-01-07 01:44:26 +0800

    style(Pagination): 調整頁籤樣式

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -1,4 +1,5 @@
 /** 套件 */
+import cx from 'classnames';
 import { Link } from "react-router-dom";
 
 import style from './style.module.scss';
@@ -6,11 +7,18 @@ import style from './style.module.scss';
 export const Pagination = ({ page, lastPage }) => {
   return (
     <ul className={style.Pagination}>
-      <li>{page > 1 && <Link to={`/?page=${page - 1}`}>上一頁</Link>}</li>
+      <li>{page > 1 && <Link to={`/?page=${page - 1}`}>«</Link>}</li>
       {[...Array(lastPage).keys()].map(i =>
-        <li key={i}><Link to={`/?page=${i + 1}`}>{i + 1}</Link></li>
+        <li key={i}>
+          <Link
+            to={`/?page=${i + 1}`}
+            className={cx({
+              [style.current]: page === i + 1
+            })}
+          >{i + 1}</Link>
+        </li>
       )}
-      <li>{page < lastPage && <Link to={`/?page=${page + 1}`}>下一頁</Link>}</li>
+      <li>{page < lastPage && <Link to={`/?page=${page + 1}`}>»</Link>}</li>
     </ul>
   )
 }
