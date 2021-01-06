7845f41b3d3ac1afaa710affb948542eff398fe7
sasie59 <sasie59@gmail.com>
2021-01-06 22:48:38 +0800

    1.feat(HomePage): 上下頁切換、影片資訊、點擊進入播放頁

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,50 +1,62 @@
 import { useEffect, useState } from 'react';
 
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import style from './style.module.scss';
 
 const { REACT_APP_KEY: KEY } = process.env;
 
+let nextPageToken;
+let prevPageToken;
+
+const POPULAR_URL = `https://www.googleapis.com/youtube/v3/videos?key=${KEY}&chart=mostPopular&maxResults=12&part=snippet,contentDetails,statistics`
+
 export const HomePage = () => {
 
+  const location = useLocation();
   const [ videoList, setVideoList ] = useState([]);
 
   useEffect(() => {
-    fetch(`https://www.googleapis.com/youtube/v3/videos?chart=mostPopular&maxResults=100&part=snippet,contentDetails,statistics&key=${KEY}`)
+    const urlParams = new URLSearchParams(location.search);
+    let myParam = urlParams.get('pageToken');
+    myParam = myParam ? `&pageToken=${myParam}`: '';
+
+    fetch(`${POPULAR_URL}${myParam}`)
       .then(res => res.json())
       .then(({ items, ...results }) => {
         console.warn(results);
+        console.warn(items[0]);
+        nextPageToken = results.nextPageToken;
+        prevPageToken = results.prevPageToken;
         setVideoList(items);
       });
-  }, []);
+  }, [location]);
 
   return (
     <div className={style.HomePage}>
+      {nextPageToken}
+      {prevPageToken}
       <ul>
         <li><Link to="/">首頁</Link></li>
         <li><Link to="/favorite">收藏頁</Link></li>
         <li><Link to="/play">播放頁</Link></li>
       </ul>
       <h1>首頁</h1>
+      <div>{prevPageToken && <Link to={`/?pageToken=${prevPageToken}`}>上一頁</Link>}</div>
+      <div>{nextPageToken && <Link to={`/?pageToken=${nextPageToken}`}>下一頁</Link>}</div>
+      <hr/>
       {videoList.map(video =>
-        <div key={video.id} className={style.VideoItem}>
-          <img src={video.snippet.thumbnails.default.url} alt={video.snippet.title} />
-        </div>
+        <Link key={video.id} to={`/play?v=${video.id}`}>
+          <div className={style.VideoItem}>
+            <img src={video.snippet.thumbnails.default.url} alt={video.snippet.title} />
+            <div className={style.title}>{video.snippet.title}</div>
+            <div className={style.description}>{video.snippet.description}</div>
+            <div className={style.duration}>{video.contentDetails.duration}</div>
+          </div>
+        </Link>
       )}
       <ol>
-        <li>每頁顯示12部影片</li>
         <li>收藏功能</li>
-        <li>
-          影片資訊需有包含
-          <ol>
-            <li>圖片</li>
-            <li>影片長度</li>
-            <li>影片標題</li>
-            <li>影片描述</li>
-          </ol>
-        </li>
-        <li>點選影片進入播放頁</li>
         <li>頁籤：總計列出100部影片，用頁籤切換顯示項目</li>
       </ol>
     </div>
