4aba1337961d3499700c43d3426a3ca5b523dd7b
sasie59 <sasie59@gmail.com>
2021-01-06 09:10:41 +0800

    feat(HomePage): load yt popular video list

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,14 +1,37 @@
+import { useEffect, useState } from 'react';
+
 import { Link } from "react-router-dom";
 
+import style from './style.module.scss';
+
+const { REACT_APP_KEY: KEY } = process.env;
+
 export const HomePage = () => {
+
+  const [ videoList, setVideoList ] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://www.googleapis.com/youtube/v3/videos?chart=mostPopular&maxResults=100&part=snippet,contentDetails,statistics&key=${KEY}`)
+      .then(res => res.json())
+      .then(({ items, ...results }) => {
+        console.warn(results);
+        setVideoList(items);
+      });
+  }, []);
+
   return (
-    <div>
+    <div className={style.HomePage}>
       <ul>
         <li><Link to="/">首頁</Link></li>
         <li><Link to="/favorite">收藏頁</Link></li>
         <li><Link to="/play">播放頁</Link></li>
       </ul>
       <h1>首頁</h1>
+      {videoList.map(video =>
+        <div key={video.id} className={style.VideoItem}>
+          <img src={video.snippet.thumbnails.default.url} alt={video.snippet.title} />
+        </div>
+      )}
       <ol>
         <li>每頁顯示12部影片</li>
         <li>收藏功能</li>
